88dc189159bc1abba8b09239a42f345d7f076039
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 19:09:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_faves)
+  my_favorite_things.concat(more_faves)
+end
+
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, "Python")
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors, name)
+  instructors.delete("Steven")
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots.delete_at(2)
+end  
