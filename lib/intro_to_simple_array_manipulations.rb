1ded17eaf70fcd9df0b7cb45d413037b6e174b00
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 18:49:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_concat(my_favorite_things, more_faves)
   my_favorite_things.concat(more_faves)
 end
+
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4)
+end 
